bfe8e8c98bb9408be6b33acce0be25bc79eafb3c
Chris Hogan <christopher.hogan@intel.com>
2017-03-31 11:42:37 -0500

    Correct year in copyright

--- cython_icc_mkl/cy.sh
+++ cython_icc_mkl/cy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-# Copyright (c) <YEAR>, Intel Corporation
+# Copyright (c) 2017, Intel Corporation
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
